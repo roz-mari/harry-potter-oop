f65205bd47cb8513539ca360129447a35a2a4457
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-05-22 00:23:40 +0200

    Added exercise OOP

--- src/main/java/hogwarts/Hogwarts.java
+++ src/main/java/hogwarts/Hogwarts.java
@@ -2,14 +2,12 @@ package hogwarts;
 
 public class Hogwarts {
     public static void main(String[] args) {
-        // Волшебники
         Wizard harry = new Harry();
         Wizard draco = new Draco();
 
         harry.castSpell("Expelliarmus");
         draco.castSpell("Serpensortia");
 
-        // Летающие объекты
         Flyable[] flyers = {
                 new Hedwig(),
         };
